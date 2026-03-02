8ee2c05e2d4ba908db357f4711b1f062b35448ca
Dimitar Terziev <midag@abv.bg>
2026-03-02 12:14:06 +0200

    Refactor routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import PokemonList from './components/PokemonList';
 import PokemonDetails from './components/PokemonDetails';
 import About from './pages/About';
@@ -7,10 +7,11 @@ import About from './pages/About';
 function App() {
  return (
     <Router>
-      <Routes basename="/Pokedex">
-        <Route path="/Pokedex" exact element={<PokemonList />} />
-        <Route path="/Pokedex/pokemon/:name" element={<PokemonDetails />} />
-        <Route path="/Pokedex/about" element={<About />} />
+      <Routes>
+        <Route path="/" element={<PokemonList />} />
+        <Route path="/pokemon/:name" element={<PokemonDetails />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Pokedex/*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
  );
